46b75ed32910031f6dc1885974edc2b8f41298d0
CmndrB <89933918+CmndrB@users.noreply.github.com>
2021-10-18 23:03:28 -0400

    Update assignment03.js

--- assignments/assignment03.js
+++ assignments/assignment03.js
@@ -16,9 +16,12 @@ for(let i=1; i<=18; i++) {
   // console.log(i);
   elem[i] = document.getElementById(i.toString());
   elem[i].children[4].children[0].onclick = function(){add1(elem[i]);};
-  elem[i].children[4].children[1].onclick = function(){subtract1(elem[i]);};
-  /*elem[i].children[4].children[2].onclick = function(){clear(elem[i]);};*/
+  /*elem[i].children[4].children[1].onclick = function(){subtract1(elem[i]);};
+  elem[i].children[4].children[2].onclick = function(){clear(elem[i]);};*/
 }
+
+elem[19] = document.getElementById("20");
+elem[19].onclick = function(){subtract1(elem[19]);};
 
 // create an "add1" function
 function add1 (elem) {
